ed0ddd149476de697c9ae2d69a5e13e81a978639
TejaPulicharla <122295002+tejapulicharla@users.noreply.github.com>
2025-12-18 12:55:53 +0530

    Comment out pipeline and add success message
    Comment out the pipeline structure and add a success message.

--- s1.groovy
+++ s1.groovy
@@ -1,11 +1,12 @@
-pipeline{
-    agent any
+//pipeline{
+  //  agent any
 // hi there 
-    stages{
-            stage ('test'){
-                echo 'to test the env'
+    //stages{
+      //      stage ('test'){
+        //        echo 'to test the env'
 
-            }
-        }
+          //  }
+        //}
 
-    }
+    //}
+echo " successfully learned how to configure these things in Github and jenkins "
